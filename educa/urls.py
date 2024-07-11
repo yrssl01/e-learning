5b62ee2c9adaf911591e2f53d4b184c496f613f2
Yerassyl <erasik01@gmail.com>
2024-07-12 01:51:16 +0500

    feat: added cms, module and contents

--- educa/urls.py
+++ educa/urls.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path(
@@ -12,6 +12,7 @@ urlpatterns = [
         'accounts/logout/', auth_views.LogoutView.as_view(), name='logout'
     ),
     path('admin/', admin.site.urls),
+    path('course/', include('courses.urls'))
 ]
 
 if settings.DEBUG:
