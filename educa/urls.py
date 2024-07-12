bad0cbf55c199c84e75a5719b6a5865ccfce984c
Yerassyl <erasik01@gmail.com>
2024-07-12 07:00:39 +0500

    feat: added courses list

--- educa/urls.py
+++ educa/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
+from courses.views import CourseListView
 
 urlpatterns = [
     path(
@@ -12,7 +13,8 @@ urlpatterns = [
         'accounts/logout/', auth_views.LogoutView.as_view(), name='logout'
     ),
     path('admin/', admin.site.urls),
-    path('course/', include('courses.urls'))
+    path('course/', include('courses.urls')),
+    path('', CourseListView.as_view(), name='course_list'),
 ]
 
 if settings.DEBUG:
